9f12da0768b5dc822693da4c5b381050be57857e
DevFC-M <felipech3344@gmail.com>
2024-03-05 21:55:48 -0300

    Commit incial de criação do projeto com o menu de escolha

---
+++ agenda.py
@@ -0,0 +1,14 @@
+while True:
+    print("\nMenu da agenda: ")
+    print("1. Adicionar contato")
+    print("2. Visualizar contatos cadastrados")
+    print("3. Editar contato")
+    print("4. Marcar / Desmarcar contato como favorito")
+    print("5. Visualizar contatos favoritos")
+    print("6. Apagar contato")
+    print("7. Exit")
+
+    escolha = input("Digite a opção desejada: ")
+
+    if escolha == "7":
+        break
